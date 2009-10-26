74ab0805ef2b06b05c91225ac4cae83d255978aa
Zachary Carter <zack.carter@gmail.com>
2009-10-25 22:55:23 -0400

    Initial semantic action support

--- parse.js
+++ parse.js
@@ -1,3 +1,7 @@
+// LARL(1), LR(1) Parser
+// Zachary Carter <zcarter@mail.usf.edu> (http://zaa.ch)
+
+load("set.js");
 
 var JSParse = exports.JSParse = (function (){
 
@@ -10,7 +14,11 @@ function Rule(sym, handle, action) {
   this.sym = sym;
   this.handle = handle;
   this.first = new Set();
-  this.action = action;
+  if(action){
+    var a = action.replace(/\$(?:0|\$)/g, "yyval")
+                  .replace(/\$(\d)/g, "stack[stack.length-$1]");
+    this.action = Function("yyval", a);
+  }
 }
   Rule.prototype.toString = function(){
     return this.sym+" -> "+this.handle.join(' ');
@@ -45,74 +53,6 @@ function LRItem(rule, dot, f) {
   LRItem.prototype.eq = function(e){
     return e.rule && e.dotPosition !=null && this.rule===e.rule && this.dotPosition === e.dotPosition && this.follows.toString()==e.follows.toString();
   };
-
-function Set(set, raw) {
-  if(set && set.constructor === Array)
-    this._items = set && (raw ? set : set.slice(0)) || [];
-  else 
-    this._items = [].slice.call(arguments,0);
-
-  var that = this;
-
-  this.length = {toValue: function(){return that.size(); }};
-}
-  Set.prototype = {
-    concat : function (setB){ 
-               return [].push.apply(this._items, setB._items), this; 
-             },
-    eq : function (set){
-            return this.size() === set.size() && this.subset(set); 
-          },
-    indexOf : function (item){
-            if(item.eq) {
-              for(var k=0; k<this._items.length;k++)
-                if(item.eq(this._items[k]))
-                  return k;
-            }
-            return this._items.indexOf(item);
-          },
-    union : function(set){
-              return (new Set(this._items)).concat(this.complement(set));
-            },
-    intersection : function(set){
-              return this.filter(function(elm){
-                    return set.contains(elm);
-                    });
-            },
-    complement : function(set){
-              var that = this;
-              return set.filter(function(elm){
-                    return !that.contains(elm);
-                    });
-            },
-    subset : function(set){
-              return this.every(function(elm){
-                    return set.contains(elm);
-                    });
-            },
-    superset : function(set){
-              return set.subset(this);
-            },
-    joinSet : function(set){
-              return this.concat(this.complement(set));
-            },
-    contains : function (item){ return this.indexOf(item) !== -1; },
-    item : function (v, val){ return this._items[v]; },
-    i : function (v, val){ return this._items[v]; },
-    first : function (){ return this._items[0]; },
-    last : function (){ return this._items[this._items.length-1]; },
-    size : function (){ return this._items.length; },
-    isEmpty : function (){ return this._items.length === 0; },
-    copy : function (){ return new Set(this._items); },
-    toString : function (){ return this._items.toString(); }
-  };
-
-  "push shift forEach some every join".split(' ').forEach(function(e,i){
-    Set.prototype[e] = function(){ return Array.prototype[e].apply(this._items, arguments); };
-  });
-  "filter slice".split(' ').forEach(function(e,i){
-    Set.prototype[e] = function(){ return new Set(Array.prototype[e].apply(this._items, arguments), true); };
-  });
 
 function NonTerminal(sym){
   this.sym = sym;
@@ -507,6 +447,7 @@ function parse(input){
   input = input.slice(0);
   input.push(this.EOF);
   var stack = [0];
+  var vstack = []; // semantic value stack
 
   var table = this.table;
 
@@ -519,7 +460,7 @@ function parse(input){
   });
 
 
-  var sym, output, state, action, a;
+  var sym, output, state, action, a, r, yyval;
   while(input){
     log('stack:',stack, '\n\t\t\tinput:', input);
     // set first input
@@ -550,8 +491,9 @@ function parse(input){
         // goto new state = table[STATE][NONTERMINAL]
         newState = table[stack[stack.length-2]][stack[stack.length-1]];
         stack.push(newState);
-        if(p.action)
-          print(p, p.action);
+        if(p.action && (r = p.action(yyval, vstack)) != undefined ){
+          return r;
+        }
         log('reduced by: ',p);
         break;
       case 'a':
