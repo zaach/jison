77752c7ac158b8c18034d8f9c73b966d2ae1b98f
Zachary Carter <zack.carter@gmail.com>
2009-11-26 23:14:29 -0500

    Fix bug in LALR parser, add JSON parsing as test case

--- tests/parser/lalr.js
+++ tests/parser/lalr.js
@@ -71,3 +71,72 @@ exports["test LR parse"] = function () {
     assert.ok(parser.parse("0+0+0"), "parse");
 };
 
+exports["test basic JSON grammer"] = function () {
+    var grammer = {
+        "lex": {
+            "macros": {
+                "digit": "[0-9]"
+            },
+            "rules": [
+                ["\\s+", "/* skip whitespace */"],
+                ["{digit}+(\\.{digit}+)?", "return 'NUMBER';"],
+                ["\"[^\"]*", function(){
+                    if(this.yytext.charAt(this.yyleng-1) == '\\') {
+                        // remove escape
+                        this.yytext = this.yytext.substr(0,this.yyleng-2);
+                        this.more();
+                    } else {
+                        this.yytext = this.yytext.substr(1); // swallow start quote
+                        this.input(); // swallow end quote
+                        return "STRING";
+                    }
+                }],
+                ["\\{", "return '{'"],
+                ["\\}", "return '}'"],
+                ["\\[", "return '['"],
+                ["\\]", "return ']'"],
+                [",", "return ','"],
+                [":", "return ':'"],
+                ["true\\b", "return 'TRUE'"],
+                ["false\\b", "return 'FALSE'"],
+                ["null\\b", "return 'NULL'"]
+            ]
+        },
+
+        "tokens": "STRING NUMBER { } [ ] , : TRUE FALSE NULL",
+        "bnf": {
+            "JsonThing": [ "JsonObject",
+                           "JsonArray" ],
+
+            "JsonObject": [ "{ JsonPropertyList }" ],
+
+            "JsonPropertyList": [ "JsonProperty",
+                                  "JsonPropertyList , JsonProperty" ],
+
+            "JsonProperty": [ "StringLiteral : JsonValue" ],
+
+            "JsonArray": [ "[ JsonValueList ]" ],
+
+            "JsonValueList": [ "JsonValue",
+                               "JsonValueList , JsonValue" ],
+
+            "JsonValue": [ "StringLiteral",
+                           "NumericalLiteral",
+                           "JsonObject",
+                           "JsonArray",
+                           "TRUE",
+                           "FALSE",
+                           "NULL" ],
+
+            "StringLiteral": [ "STRING" ],
+
+            "NumericalLiteral": [ "NUMBER" ]
+        },
+    };
+
+    var source = '{"foo": "Bar", "hi": 42, "array": [1,2,3.004,4], "false": false, "true":true, "null": null, "obj": {"ha":"ho"}, "string": "string\\"sgfg" }';
+
+    var parser = new Jison.Parser(grammer);
+    assert.ok(parser.parse(source));
+}
+
