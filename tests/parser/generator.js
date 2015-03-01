zaach/jison

commit 9e0cc65383fe05bda8f49f79f553cc31b0d0e24e

refactor generation so that in-memory and to-source code paths are the same

lib/jison.js
@@ -253,13 +253,7 @@ generator.buildProductions = function buildProductions(bnf, productions, nonterm
     var parameters = "yytext, yyleng, yylineno, yy, yystate /* action[1] */, $$ /* vstack */, _$ /* lstack */";
     if (this.parseParams) parameters += ', ' + this.parseParams.join(', ');
 
-    // first try to create the performAction function the old way,
-    // but this will break for some legal constructs in the user action code:
-    try {
-        this.performAction = Function(parameters, actions);
-    } catch (e) {
-        this.performAction = "function anonymous(" + parameters + ") {\n" + actions + "\n}";
-    }
+    this.performAction = "function anonymous(" + parameters + ") {\n" + actions + "\n}";
 
     function buildProduction (handle) {
         var r, rhs, i;
@@ -1054,7 +1048,17 @@ lrGeneratorMixin.generateModule = function generateModule (opt) {
         + "    recoverable: (boolean: TRUE when the parser has a error recovery rule available for this particular error)\n"
         + "  }\n"
         + "*/\n";
-    out += (moduleName.match(/\./) ? moduleName : "var "+moduleName)+" = (function(){";
+    out += (moduleName.match(/\./) ? moduleName : "var "+moduleName) +
+            " = " + this.generateModuleExpr();
+
+    return out;
+};
+
+
+lrGeneratorMixin.generateModuleExpr = function generateModuleExpr () {
+    var out = '';
+
+    out += "(function(){";
     out += "\nvar parser = "+this.generateModule_();
     out += "\n"+this.moduleInclude;
     if (this.lexer && this.lexer.generateModule) {
@@ -1157,16 +1161,8 @@ var lrGeneratorDebug = {
 var parser = typal.beget();
 
 lrGeneratorMixin.createParser = function createParser () {
-    var p = parser.beget();
-    p.yy = {};
 
-    p.init(this);
-
-    // don't throw if grammar recovers from errors
-    if (this.hasErrorRecovery) {
-        p.parseError = traceParseError;
-        p.recover = true;
-    }
+    var p = eval(this.generateModuleExpr());
 
     // for debugging
     p.productions = this.productions;
@@ -1180,15 +1176,10 @@ lrGeneratorMixin.createParser = function createParser () {
     }
 
     // backwards compatability
-    p.lexer = this.lexer;
     p.generate = bind('generate');
     p.generateAMDModule = bind('generateAMDModule');
     p.generateModule = bind('generateModule');
     p.generateCommonJSModule = bind('generateCommonJSModule');
-
-    p.Parser = function () {
-      return self.createParser();
-    };
 
     return p;
 };

commit 7bcf8b7ed7337939b31a207881eba1c61ae47018

Fix this.lexer undefined, fixes #246
It seems that 9e0cc65 introduced a bug in the generated
code, such that `this.lexer` is undefined, and so it
throws a runtime error.

This fixes the bug, and also adds a test to reproduce the
error. The test fails before the fix, but succeeds after.
I ran the tests and confirmed 100% success and no errors.

lib/jison.js
@@ -1338,6 +1338,7 @@ lrGeneratorMixin.createParser = function createParser () {
     }
 
     // backwards compatability
+    p.lexer = this.lexer;
     p.generate = bind('generate');
     p.generateAMDModule = bind('generateAMDModule');
     p.generateModule = bind('generateModule');

tests/parser/generator.js
@@ -2,6 +2,9 @@ var Jison = require("../setup").Jison,
     Lexer = require("../setup").Lexer,
     assert = require("assert");
 
+var fs = require('fs');
+var path = require('path');
+
 exports["test amd module generator"] = function() {
     var lexData = {
         rules: [
@@ -345,4 +348,34 @@ exports["test module include with each generator type"] = function () {
       var source = gen[type]();
       assert.ok(/TEST_VAR/.test(source), type + " supports module include");
     });
+};
+
+// test for issue #246
+exports["test compiling a parser/lexer"] = function () {
+    var grammar =
+      '// Simple "happy happy joy joy" parser, written by Nolan Lawson\n' +
+      '// Based on the song of the same name.\n\n' +
+      '%lex\n%%\n\n\\s+                   /* skip whitespace */\n' +
+      '("happy")             return \'happy\'\n' +
+      '("joy")               return \'joy\'\n' +
+      '<<EOF>>               return \'EOF\'\n\n' +
+      '/lex\n\n%start expressions\n\n' +
+      '%ebnf\n\n%%\n\n' +
+      'expressions\n    : e EOF\n        {return $1;}\n    ;\n\n' +
+      'e\n    : phrase+ \'joy\'? -> $1 + \' \' + yytext \n    ;\n\n' +
+      'phrase\n    : \'happy\' \'happy\' \'joy\' \'joy\' ' +
+      ' -> [$1, $2, $3, $4].join(\' \'); \n    ;';
+
+    var parser = new Jison.Parser(grammar);
+    var generated = parser.generate();
+
+    var tmpFile = path.resolve(__dirname, 'tmp-parser.js');
+    fs.writeFileSync(tmpFile, generated);
+    var parser2 = require('./tmp-parser');
+
+    assert.ok(parser.parse('happy happy joy joy joy') === 'happy happy joy joy joy',
+      'original parser works');
+    assert.ok(parser2.parse('happy happy joy joy joy') === 'happy happy joy joy joy',
+      'generated parser works');
+    fs.unlinkSync(tmpFile);
 };