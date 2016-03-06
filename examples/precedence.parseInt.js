9dc5c76b4ca94d92316c0d4c065df0ed78c2c27e
Ger Hobbelt <ger@hobbelt.com>
2016-03-06 19:28:06 +0100

    updated examples

--- examples/precedence.parseInt.js
+++
@@ -1,9 +0,0 @@
-// given the grammar rule
-// 
-//       | NAT
-//
-// any of the alternative codings below will work, while the last is adviced:
-
-$$ = parseInt(yytext);
-$$ = parseInt($1);
-$$ = parseInt($NAT);
--- examples/precedence.returnNAT.js
+++
@@ -1,5 +0,0 @@
-// the lexer generator code will look at this action block and correctly replace the string token
-// return by a nice & fast numeric token ID, just like would've happened if this code had been sitting 
-// inside a `%{...%}` or `{...}` ACTION block:
-
-return 'NAT';
