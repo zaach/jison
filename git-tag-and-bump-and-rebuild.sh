93221623e314f0d9224af89a713a4024357a0444
Ger Hobbelt <ger@hobbelt.com>
2017-01-30 21:14:51 +0100

    rebuilt library files

--- git-tag-and-bump-and-rebuild.sh
+++ git-tag-and-bump-and-rebuild.sh
@@ -4,50 +4,189 @@
 pushd $(dirname $0)                                                                                     2> /dev/null  > /dev/null
 
 
-make git-tag
-make site
+# ---------------------------------------------------------------------------
+# stage 1: rebuild all libraries from scratch, using existing NPM packages.
+#          Commit. (`npm install` fails often enough that we invoke the bastard TWICE!)
+# ---------------------------------------------------------------------------
 
-../../../util/git_pull_push.sh -f
+make superclean ; make prep ; make prep ; make site
 
-ncu -a --packageFile package.json 
-
-make superclean ; make prep ; make site
 
 # git submodule foreach git commit -a -m 'rebuilt library files'
 # ^-- one or more subrepo's may fail as there'ld be nothing to commit,
 #     which would abort the `git submodule foreach` command!
 pushd modules/ebnf-parser/                                                                                     2> /dev/null  > /dev/null
 git commit -a -m 'rebuilt library files'
+git push --all
 popd                                                                                                           2> /dev/null  > /dev/null
 pushd modules/jison2json/                                                                                      2> /dev/null  > /dev/null
 git commit -a -m 'rebuilt library files'
+git push --all
 popd                                                                                                           2> /dev/null  > /dev/null
 pushd modules/jison-lex/                                                                                       2> /dev/null  > /dev/null
 git commit -a -m 'rebuilt library files'
+git push --all
 popd                                                                                                           2> /dev/null  > /dev/null
 pushd modules/json2jison/                                                                                      2> /dev/null  > /dev/null
 git commit -a -m 'rebuilt library files'
+git push --all
 popd                                                                                                           2> /dev/null  > /dev/null
 pushd modules/lex-parser/                                                                                      2> /dev/null  > /dev/null
 git commit -a -m 'rebuilt library files'
+git push --all
 popd                                                                                                           2> /dev/null  > /dev/null
 
 git commit -a -m 'rebuilt library files'
+git push --all
+
+
+make superclean ; make prep ; make prep ; make site
+
+
+pushd modules/ebnf-parser/                                                                                     2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison2json/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison-lex/                                                                                       2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/json2jison/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/lex-parser/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+
+git commit -a -m 'rebuilt library files'
+git push --all
+
+
+# ---------------------------------------------------------------------------
+# stage 2: Tag and Publish.
+# ---------------------------------------------------------------------------
+
+
+make git-tag
 
 npm publish
 
-# ----
+
+# ---------------------------------------------------------------------------
+# stage 3: Bump build revision for future work, commit & push.
+# ---------------------------------------------------------------------------
+
 
 make bump
 
-git submodule foreach git commit -a -m 'bumped build revision'
+
+# git submodule foreach git commit -a -m 'rebuilt library files'
+# ^-- one or more subrepo's may fail as there'ld be nothing to commit,
+#     which would abort the `git submodule foreach` command!
+pushd modules/ebnf-parser/                                                                                     2> /dev/null  > /dev/null
 git commit -a -m 'bumped build revision'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison2json/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'bumped build revision'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison-lex/                                                                                       2> /dev/null  > /dev/null
+git commit -a -m 'bumped build revision'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/json2jison/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'bumped build revision'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/lex-parser/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'bumped build revision'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
 
-../../../util/git_pull_push.sh -f
+git commit -a -m 'bumped build revision'
+git push --all
 
-# ncu -a --packageFile package.json 
 
-make superclean ; make prep ; make site
+# ---------------------------------------------------------------------------
+# stage 4: update NPM packages, if any; rebuild & commit
+# ---------------------------------------------------------------------------
+
+
+# rebuild everything before we go and tag the buggers!
+#make git-tag
+#make site
+
+#../../../util/git_pull_push.sh -f
+
+pushd modules/ebnf-parser/                                                                                     2> /dev/null  > /dev/null
+ncu -a --packageFile package.json 
+git commit -a -m 'updated NPM packages'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison2json/                                                                                      2> /dev/null  > /dev/null
+ncu -a --packageFile package.json 
+git commit -a -m 'updated NPM packages'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison-lex/                                                                                       2> /dev/null  > /dev/null
+ncu -a --packageFile package.json 
+git commit -a -m 'updated NPM packages'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/json2jison/                                                                                      2> /dev/null  > /dev/null
+ncu -a --packageFile package.json 
+git commit -a -m 'updated NPM packages'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/lex-parser/                                                                                      2> /dev/null  > /dev/null
+ncu -a --packageFile package.json 
+git commit -a -m 'updated NPM packages'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+
+ncu -a --packageFile package.json 
+git commit -a -m 'updated NPM packages'
+git push --all
+
+
+make superclean ; make prep ; make prep ; make site
+
+
+pushd modules/ebnf-parser/                                                                                     2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison2json/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison-lex/                                                                                       2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/json2jison/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/lex-parser/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+git push --all
+popd                                                                                                           2> /dev/null  > /dev/null
+
+git commit -a -m 'rebuilt library files'
+git push --all
+
+
+
+echo "Done. You can now continue work on the new version:"
+node lib/cli.js -V
 
 
 popd                                                                                                    2> /dev/null  > /dev/null
