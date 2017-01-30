248c2b623523a3719b2d3661b7d12de95317c4b5
Ger Hobbelt <ger@hobbelt.com>
2017-01-30 20:42:08 +0100

    adjusted tag+versionbump shell build script to ensure the tagged version is used to rebuild the jison tool and then the result gets published on NPM. (yep, there's a small risk that process fails, in which case we have to fix the bug and issue a new version on NPM; not entirely happy with this process yet, but it's a wee bit better than what I got before...)

--- git-tag-and-bump-and-rebuild.sh
+++ git-tag-and-bump-and-rebuild.sh
@@ -5,15 +5,47 @@ pushd $(dirname $0)                                                             
 
 
 make git-tag
-make bump
 make site
+
+../../../util/git_pull_push.sh -f
+
+ncu -a --packageFile package.json 
+
+make superclean ; make prep ; make site
+
+# git submodule foreach git commit -a -m 'rebuilt library files'
+# ^-- one or more subrepo's may fail as there'ld be nothing to commit,
+#     which would abort the `git submodule foreach` command!
+pushd modules/ebnf-parser/                                                                                     2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison2json/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/jison-lex/                                                                                       2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/json2jison/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+popd                                                                                                           2> /dev/null  > /dev/null
+pushd modules/lex-parser/                                                                                      2> /dev/null  > /dev/null
+git commit -a -m 'rebuilt library files'
+popd                                                                                                           2> /dev/null  > /dev/null
+
+git commit -a -m 'rebuilt library files'
+
+npm publish
+
+# ----
+
+make bump
 
 git submodule foreach git commit -a -m 'bumped build revision'
 git commit -a -m 'bumped build revision'
 
 ../../../util/git_pull_push.sh -f
 
-ncu -a --packageFile package.json 
+# ncu -a --packageFile package.json 
 
 make superclean ; make prep ; make site
 
