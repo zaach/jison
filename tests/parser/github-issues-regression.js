1ce7eafd0e57d75627b3f0286b95b15f81573c4b
Ger Hobbelt <ger@hobbelt.com>
2017-10-13 18:39:26 +0200

    fix unit test (copy-pasta cruft removal)

--- tests/parser/github-issues-regression.js
+++ tests/parser/github-issues-regression.js
@@ -7,11 +7,19 @@ var helpers     = require('../../modules/helpers-lib');
 var code_exec   = helpers.exec;
 
 
+var original_cwd = process.cwd();
+
+
 describe("Regression Checks", function () {
   it("GitHub JISON issue #13: https://github.com/GerHobbelt/jison/issues/13", function () {
+    // Change CWD to the directory where the source grammar resides: this helps us properly
+    // %include any files mentioned in the grammar with relative paths:
+    process.chdir(__dirname);
+
     var filespec = {
-      path: "tests/parser/github-issues-nr-13-gho.y"
+      path: "./github-issues-nr-13-gho.y"
     };
+
     var grammar = fs.readFileSync(filespec.path, "utf8");
 
     var options = {
@@ -24,30 +32,8 @@ describe("Regression Checks", function () {
 
       assert.ok(false, "should never get here");
     } catch (ex) {
-      console.log("ex:", ex);
-      assert.ok(false);
-    }
-    code_exec(String(parser.parse), function test_exec() {
-      if (typeof parser.main === 'function') {
-        assert.ok(!parser.main(), 'main() is supposed to produce zero ~ success');
-      } else if (filespec.inputs) {
-        for (var i = 0, l = filespec.inputs.length; i < l; i++) {
-          rv = parser.parse(filespec.inputs[i]);
-          console.log('parse A: ', filespec.inputs[i], rv);
-          assert.strictEqual(rv, true, 'parser.parse() is supposed to produce TRUE');
-        }
-      } else {
-        rv = parser.parse('zz; yy; zz;zz ;');
-        console.log('parse B: ', path.basename(filespec.path), rv);
-        assert.strictEqual(rv, true, 'parser.parse() is supposed to produce TRUE');
-      }
-    }, {
-      dumpSourceCodeOnFailure: true,
-      throwErrorOnCompileFailure: true,
-    }, "test");
-
-    if (filespec.__check__) {
-      filespec.__check__(parser, filespec, rv, options.exportAllTables);
+      throw new Error("ex:" + ex + ex.stack);
+      assert.ok(true);
     }
   });
 });
