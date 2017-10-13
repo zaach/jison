7b47bc3203b5b6c4e32d6e4d965f96c76d3bf9fb
Ger Hobbelt <ger@hobbelt.com>
2017-10-13 20:40:39 +0200

    as with jison itself, here rollup+babel also damage the lexer runtime kernel too much to keep using the `String(function-code)` trick, so we revert to patching a stringified version of the kernel into the library proper, just like we do in jison (lib/jison.js) itself!

---
+++ packages/jison-lex/__patch_lexer_kernel_in_js.js
@@ -0,0 +1,39 @@
+
+const globby = require('globby');
+const fs = require('fs');
+
+var kernel = fs.readFileSync('jison-lexer-kernel.js', 'utf8');
+kernel = kernel
+.replace(/\\/g, '\\\\')
+.replace(/`/g, '\\`')
+.trim();
+ 
+globby(['regexp-lexer.js']).then(paths => {
+	var count = 0;
+
+    //console.log(paths);
+    paths.forEach(path => {
+    	var updated = false;
+
+    	//console.log('path: ', path);
+
+    	var src = fs.readFileSync(path, 'utf8');
+    	src = src.replace(/(\/\/ --- START lexer kernel ---)[^]+?(\/\/ --- END lexer kernel ---)/, function f(m, p1, p2) {
+            return p1 + `
+return \`${kernel}\`;
+` + p2;
+        });
+		updated = true;
+
+    	if (updated) {
+    		count++;
+    		console.log('updated: ', path);
+	    	fs.writeFileSync(path, src, {
+                encoding: 'utf8',
+                flags: 'w'
+            });
+	    }
+    });
+
+    console.log('\nUpdated', count, 'files\' lexer kernel core code.');
+});
