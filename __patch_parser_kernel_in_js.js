6f9a8b612a5d7f14bace314d35e78f4726c03893
Ger Hobbelt <ger@hobbelt.com>
2017-12-23 01:55:17 +0100

    move stringified code chunks outside jison, like we already did for the parser kernel et al: then use the `__patch_parser_kernel_in_js.js` utility to patch those master sources back into jison.js.

--- __patch_parser_kernel_in_js.js
+++ __patch_parser_kernel_in_js.js
@@ -2,23 +2,27 @@
 const globby = require('globby');
 const fs = require('fs');
 
-var kernel = fs.readFileSync('lib/jison-parser-kernel.js', 'utf8');
-kernel = kernel
-.replace(/\\/g, '\\\\')
-.replace(/`/g, '\\`')
-.trim();
+function encode(str) {
+    return str 
+    .replace(/\\/g, '\\\\')
+    .replace(/`/g, '\\`')
+    .trim();
+}
+
+var kernel = encode(fs.readFileSync('lib/jison-parser-kernel.js', 'utf8'));
  
-var parseErrorCode = fs.readFileSync('lib/jison-parser-parseError-function.js', 'utf8');
-parseErrorCode = parseErrorCode
-.replace(/\\/g, '\\\\')
-.replace(/`/g, '\\`')
-.trim();
+var parseErrorCode = encode(fs.readFileSync('lib/jison-parser-parseError-function.js', 'utf8'));
  
-var errorClassCode = fs.readFileSync('lib/jison-parser-error-code.js', 'utf8');
-errorClassCode = errorClassCode
-.replace(/\\/g, '\\\\')
-.replace(/`/g, '\\`')
-.trim();
+var errorClassCode = encode(fs.readFileSync('lib/jison-parser-error-code.js', 'utf8'));
+ 
+var debugTraceCode = encode(fs.readFileSync('lib/jison-parser-debugTrace-function.js', 'utf8'));
+ 
+var commonJsMainCode = encode(fs.readFileSync('lib/jison-parser-commonJsMain-function.js', 'utf8'));
+ 
+var parserAPIs1Code = encode(fs.readFileSync('lib/jison-parser-API-section1.js', 'utf8'))
+    .replace(/^[^{]*\{/, '')
+    .replace(/\}[^\}]*$/, '')
+    .trim();
  
 globby(['lib/jison.js']).then(paths => {
 	var count = 0;
@@ -30,7 +34,7 @@ globby(['lib/jison.js']).then(paths => {
     	//console.log('path: ', path);
 
     	var src = fs.readFileSync(path, 'utf8');
-    	src = src
+    	var dst = src
         .replace(/(\/\/ --- START parser kernel ---)[^]+?(\/\/ --- END parser kernel ---)/, function f(m, p1, p2) {
             return p1 + `
 parser.parse = \`${kernel}\`;
@@ -38,15 +42,23 @@ parser.parse = \`${kernel}\`;
         })
         .replace(/(\/\/ --- START parser error class ---)[^]+?(\/\/ --- END parser error class ---)/, function f(m, p1, p2) {
             return p1 + `
-
-var prelude = \`${errorClassCode}\`;
-
-    ` + p2;
+    const prelude = \`
+${errorClassCode}
+\`;    ` + p2;
         })
         .replace(/(const parseErrorSourceCode = `)[^]+?(\/\/ END of parseErrorSourceCode chunk)/, function f(m, p1, p2) {
             return p1 + '\n' + parseErrorCode + '\`;    ' + p2;
+        })
+        .replace(/(const debugTraceSrc = `)[^]+?(\/\/ END of debugTraceSrc chunk)/, function f(m, p1, p2) {
+            return p1 + '\n' + debugTraceCode + '\n\`;    ' + p2;
+        })
+        .replace(/(const commonJsMain = `)[^]+?(\/\/ END of commonJsMain chunk)/, function f(m, p1, p2) {
+            return p1 + '\n' + commonJsMainCode + '\n\`;    ' + p2;
+        })
+        .replace(/(const define_parser_APIs_1 = `)[^]+?(\/\/ END of define_parser_APIs_1 chunk)/, function f(m, p1, p2) {
+            return p1 + '\n    ' + parserAPIs1Code + '\n\`;    ' + p2;
         });
-		updated = true;
+		updated = (dst !== src);
 
     	if (updated) {
     		count++;
