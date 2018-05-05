a0a5361a0c2f07e2889a7ec59f108b02ccc07fab
Ger Hobbelt <ger@hobbelt.com>
2018-05-05 13:09:54 +0200

    helpers lib: move more generic functions from inside other Jison components into here: escaping and unescaping strings, cheching wether a given regex extracted from some source is actually a legal regex and whether such regex has any `(...)` captures or not - the latter is relevant when you wish to unroll/combine multiple lexer regexes for lexer speed improvement a la the `moo` lexer generator. These APIs are now part of the helpers library:
    - isLegalIdentifierInput
    - scanRegExp
    - checkRegExp
    - getRegExpInfo

--- packages/helpers-lib/dist/helpers-lib-cjs-es5.js
+++ packages/helpers-lib/dist/helpers-lib-cjs-es5.js
@@ -121,13 +121,98 @@ function camelCase(s) {
 // Convert dashed option keys and other inputs to Camel Cased legal JavaScript identifiers
 /** @public */
 function mkIdentifier(s) {
-    s = camelCase('' + s);
+    s = '' + s;
+    return s
+    // Convert dashed ids to Camel Case (though NOT lowercasing the initial letter though!), 
+    // e.g. `camelCase('camels-have-one-hump')` => `'camelsHaveOneHump'`
+    .replace(/-\w/g, function (match) {
+        var c = match.charAt(1);
+        var rv = c.toUpperCase();
+        // do not mutate 'a-2' to 'a2':
+        if (c === rv && c.match(/\d/)) {
+            return match;
+        }
+        return rv;
+    })
     // cleanup: replace any non-suitable character series to a single underscore:
-    return s.replace(/^[^\w_]/, '_')
+    .replace(/^[^\w_]/, '_')
     // do not accept numerics at the leading position, despite those matching regex `\w`:
-    .replace(/^\d/, '_').replace(/[^\w\d_]+/g, '_')
+    .replace(/^\d/, '_').replace(/[^\w\d_]/g, '_')
     // and only accept multiple (double, not triple) underscores at start or end of identifier name:
     .replace(/^__+/, '#').replace(/__+$/, '#').replace(/_+/g, '_').replace(/#/g, '__');
+}
+
+// Check if the start of the given input matches a regex expression.
+// Return the length of the regex expression or -1 if none was found.
+/** @public */
+function scanRegExp(s) {
+    s = '' + s;
+
+    // code based on Esprima scanner: `Scanner.prototype.scanRegExpBody()`
+    var index = 0;
+    var length = s.length;
+    var ch = s[index];
+    //assert.assert(ch === '/', 'Regular expression literal must start with a slash');
+    var str = s[index++];
+    var classMarker = false;
+    var terminated = false;
+    while (index < length) {
+        ch = s[index++];
+        str += ch;
+        if (ch === '\\') {
+            ch = s[index++];
+            // https://tc39.github.io/ecma262/#sec-literals-regular-expression-literals
+            if (isLineTerminator(ch.charCodeAt(0))) {
+                return -1; // UnterminatedRegExp
+            }
+            str += ch;
+        } else if (isLineTerminator(ch.charCodeAt(0))) {
+            return -1; // UnterminatedRegExp
+        } else if (classMarker) {
+            if (ch === ']') {
+                classMarker = false;
+            }
+        } else {
+            if (ch === '/') {
+                terminated = true;
+                break;
+            } else if (ch === '[') {
+                classMarker = true;
+            }
+        }
+    }
+    if (!terminated) {
+        return -1; // UnterminatedRegExp
+    }
+    return index;
+}
+
+// https://tc39.github.io/ecma262/#sec-line-terminators
+function isLineTerminator(cp) {
+    return cp === 0x0A || cp === 0x0D || cp === 0x2028 || cp === 0x2029;
+}
+
+// Check if the given input can be a legal identifier-to-be-camelcased:
+// use this function to check if the way the identifier is written will
+// produce a sensible & comparable identifier name using the `mkIdentifier'
+// API - for humans that transformation should be obvious/trivial in
+// order to prevent confusion.
+/** @public */
+function isLegalIdentifierInput(s) {
+    s = '' + s;
+    // Convert dashed ids to Camel Case (though NOT lowercasing the initial letter though!), 
+    // e.g. `camelCase('camels-have-one-hump')` => `'camelsHaveOneHump'`
+    s = s.replace(/-\w/g, function (match) {
+        var c = match.charAt(1);
+        var rv = c.toUpperCase();
+        // do not mutate 'a-2' to 'a2':
+        if (c === rv && c.match(/\d/)) {
+            return match;
+        }
+        return rv;
+    });
+    var alt = mkIdentifier(s);
+    return alt === s;
 }
 
 // properly quote and escape the given input string
@@ -460,11 +545,130 @@ function detectIstanbulGlobal() {
     return coverage || false;
 }
 
+//
+// Helper library for safe code execution/compilation
+//
+// MIT Licensed
+//
+//
+// This code is intended to help test and diagnose arbitrary regexes, answering questions like this:
+//
+// - is this a valid regex, i.e. does it compile?
+// - does it have captures, and if yes, how many?
+//
+
+//import XRegExp from '@gerhobbelt/xregexp';
+
+
+// validate the given regex.
+//
+// You can specify an (advanced or regular) regex class as a third parameter.
+// The default assumed is the standard JavaScript `RegExp` class.
+//
+// Return FALSE when there's no failure, otherwise return an `Error` info object.
+function checkRegExp(re_src, re_flags, XRegExp) {
+    var re;
+
+    // were we fed a RegExp object or a string?
+    if (re_src && typeof re_src.source === 'string' && typeof re_src.flags === 'string' && typeof re_src.toString === 'function' && typeof re_src.test === 'function' && typeof re_src.exec === 'function') {
+        // we're looking at a RegExp (or XRegExp) object, so we can trust the `.source` member
+        // and the `.toString()` method to produce something that's compileable by XRegExp
+        // at least...
+        if (!re_flags || re_flags === re_src.flags) {
+            // no change of flags: we assume it's okay as it's already contained
+            // in an RegExp or XRegExp object
+            return false;
+        }
+    }
+    // we DO accept empty regexes: `''` but we DO NOT accept null/undefined
+    if (re_src == null) {
+        return new Error('invalid regular expression source: ' + re_src);
+    }
+
+    re_src = '' + re_src;
+    if (re_flags == null) {
+        re_flags = undefined; // `new RegExp(..., flags)` will barf a hairball when `flags===null`
+    } else {
+        re_flags = '' + re_flags;
+    }
+
+    XRegExp = XRegExp || RegExp;
+
+    try {
+        re = new XRegExp(re_src, re_flags);
+    } catch (ex) {
+        return ex;
+    }
+    return false;
+}
+
+// provide some info about the given regex.
+//
+// You can specify an (advanced or regular) regex class as a third parameter.
+// The default assumed is the standard JavaScript `RegExp` class.
+//
+// Return FALSE when the input is not a legal regex.
+function getRegExpInfo(re_src, re_flags, XRegExp) {
+    var re1, re2, m1, m2;
+
+    // were we fed a RegExp object or a string?
+    if (re_src && typeof re_src.source === 'string' && typeof re_src.flags === 'string' && typeof re_src.toString === 'function' && typeof re_src.test === 'function' && typeof re_src.exec === 'function') {
+        // we're looking at a RegExp (or XRegExp) object, so we can trust the `.source` member
+        // and the `.toString()` method to produce something that's compileable by XRegExp
+        // at least...
+        if (!re_flags || re_flags === re_src.flags) {
+            // no change of flags: we assume it's okay as it's already contained
+            // in an RegExp or XRegExp object
+            re_flags = undefined;
+        }
+    } else if (re_src == null) {
+        // we DO NOT accept null/undefined
+        return false;
+    } else {
+        re_src = '' + re_src;
+
+        if (re_flags == null) {
+            re_flags = undefined; // `new RegExp(..., flags)` will barf a hairball when `flags===null`
+        } else {
+            re_flags = '' + re_flags;
+        }
+    }
+
+    XRegExp = XRegExp || RegExp;
+
+    try {
+        // A little trick to obtain the captures from a regex:
+        // wrap it and append `(?:)` to ensure it matches
+        // the empty string, then match it against it to
+        // obtain the `match` array.
+        re1 = new XRegExp(re_src, re_flags);
+        re2 = new XRegExp('(?:' + re_src + ')|(?:)', re_flags);
+        m1 = re1.exec('');
+        m2 = re2.exec('');
+        return {
+            acceptsEmptyString: !!m1,
+            captureCount: m2.length - 1
+        };
+    } catch (ex) {
+        return false;
+    }
+}
+
+var reHelpers = {
+    checkRegExp: checkRegExp,
+    getRegExpInfo: getRegExpInfo
+};
+
 var index = {
     rmCommonWS: rmCommonWS,
     camelCase: camelCase,
     mkIdentifier: mkIdentifier,
+    isLegalIdentifierInput: isLegalIdentifierInput,
+    scanRegExp: scanRegExp,
     dquote: dquote,
+
+    checkRegExp: reHelpers.checkRegExp,
+    getRegExpInfo: reHelpers.getRegExpInfo,
 
     exec: code_exec.exec,
     dump: code_exec.dump,
